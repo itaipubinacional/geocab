50c796d911c287609fc9e136c723331659aecbc2
Cristiano Correa <cristiano@cristiano.correa>
2014-09-15 18:19:18 -0300

    Commit inicial do codigo fonte

---
+++ implementation/solution/src/main/java/br/com/geocab/domain/entity/IEntity.java
@@ -0,0 +1,27 @@
+package br.com.geocab.domain.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author Rodrigo P. Fraga 
+ * @since 22/11/2012
+ * @version 1.0
+ * @category Entity
+ */
+public interface IEntity<ID extends Serializable> extends Serializable
+{
+	
+	/*-------------------------------------------------------------------
+	 * 		 				GETTERS AND SETTERS
+	 *-------------------------------------------------------------------*/
+	/**
+	 * 
+	 * @return
+	 */
+	public ID getId();
+	/**
+	 * 
+	 * @param id
+	 */
+	public void setId(ID id);
+}
