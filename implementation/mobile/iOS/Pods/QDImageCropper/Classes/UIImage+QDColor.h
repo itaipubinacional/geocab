e331923c3dad70faa03b853fb38ee896a6d20c5d
Henrique Lobato <henriquelobato@Juciels-Mac.local>
2014-10-01 08:34:33 -0300

    Commit inicial do iOS.

---
+++ implementation/mobile/iOS/Pods/QDImageCropper/Classes/UIImage+QDColor.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+QDColor.h
+//  QDImageCropper
+//
+//  Created by Nikolay on 13/04/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (QDColor)
+
++ (UIImage *)imageWithColor:(UIColor *)color width:(double)width height:(double)height;
+
+@end
