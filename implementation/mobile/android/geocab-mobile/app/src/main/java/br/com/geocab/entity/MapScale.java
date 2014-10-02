5fc2e0efbcad52b91ff051a39c5b4c6d6da524e1
Vinicius <vinicius.kawamoto@eits.com.br>
2014-10-02 15:09:21 -0300

    criado projeto android

---
+++ implementation/mobile/android/geocab-mobile/app/src/main/java/br/com/geocab/entity/MapScale.java
@@ -0,0 +1,22 @@
+package br.com.geocab.entity;
+
+/**
+ * Created by Vinicius on 25/09/2014.
+ */
+public enum MapScale {
+
+        /*-------------------------------------------------------------------
+         *				 		     ENUMS
+         *-------------------------------------------------------------------*/
+        //Do not change this order
+        UM70M, UM35M, UM18M, UM9M, UM4M, UM2M, UM1M, UM528K, UM274K, UM137K, UM68K, UM34K, UM17K, UM8561, UM4280, UM2140;
+
+        /**
+         *
+         * @return value of enum
+         */
+        public int getOrdinal()
+        {
+            return this.ordinal();
+        }
+}
