cdbf8d834e82aa363773fd7ca208a80d6618c478
Henrique Lobato Zago <hnlbzg@gmail.com>
2014-10-06 15:28:28 -0300

    entidades adicionadas

---
+++ implementation/mobile/iOS/geocab/Marker.m
@@ -0,0 +1,13 @@
+//
+//  Marker.m
+//  geocab
+//
+//  Created by Henrique Lobato on 06/10/14.
+//  Copyright (c) 2014 Itaipu. All rights reserved.
+//
+
+#import "Marker.h"
+
+@implementation Marker
+
+@end
