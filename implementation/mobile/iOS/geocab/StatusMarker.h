960de33d24d24b130e81a8661c4d8e2f7aca379c
Henrique Lobato Zago <hnlbzg@gmail.com>
2014-10-06 15:28:28 -0300

    entidades adicionadas

---
+++ implementation/mobile/iOS/geocab/StatusMarker.h
@@ -0,0 +1,13 @@
+//
+//  StatusMarker.h
+//  geocab
+//
+//  Created by Henrique Lobato on 06/10/14.
+//  Copyright (c) 2014 Itaipu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StatusMarker : NSObject
+
+@end
