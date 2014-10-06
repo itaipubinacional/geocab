673d41d0e0514941a8504bec662aad1a0bc36adc
Henrique Lobato Zago <hnlbzg@gmail.com>
2014-10-06 09:45:58 -0300

    entidades alteradas e mmenu lateral adicionado.

---
+++ implementation/mobile/iOS/geocab/SideMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  SideMenuViewController.h
+//  geocab
+//
+//  Created by Henrique Lobato on 02/10/14.
+//  Copyright (c) 2014 Itaipu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideMenuViewController : UIViewController
+
+@end
