51e27db90001822a57716b094de6fb2af264051b
Vinicius <vinicius.kawamoto@eits.com.br>
2014-11-03 10:58:12 -0200

    criado serviço para autenticar com rede social

--- implementation/solution/src/main/java/br/com/geocab/application/restful/AuthenticationRESTful.java
+++ implementation/solution/src/main/java/br/com/geocab/application/restful/AuthenticationRESTful.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -62,6 +64,22 @@ public class AuthenticationRESTful
 			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
 			return null;
 		}
+	}
+	
+	/**
+	 * 
+	 */ 
+	@RequestMapping(value = "create", method = RequestMethod.POST)
+	public @ResponseBody User insert( @RequestParam("email") String email, @RequestParam("name") String name )
+	{
+		final User user = this.loginService.findUserByEmail(email);
+		
+		if( user != null )
+		{
+			return user;
+		}
+		
+		return this.loginService.insertUserSocial(email, name);
 	}
 	
 	
