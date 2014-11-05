512155b2cca639e56a665f2fb332044a13b1e21c
Lucas <lucas.aguiar@eits.com.br>
2014-11-05 14:41:51 -0200

    pop up para seleção de icones, e ajustes no logo da tela de login

---
+++ implementation/solution/src/main/webapp/modules/admin/controller/popup/icons-popup-controller.js
@@ -0,0 +1,104 @@
+'use strict';
+
+/**
+ * 
+ * @param $scope
+ * @param $log
+ * @param $location
+ */
+function ChoosePopupController($scope, $injector,$modalInstance, $state, attributes ) {
+
+
+	$scope.msg = null;
+
+	/*-------------------------------------------------------------------
+	 * 		 				 	ATTRIBUTES
+	 *-------------------------------------------------------------------*/
+
+    /*-------------------------------------------------------------------
+     * 		 				 	ATTRIBUTES
+     *-------------------------------------------------------------------*/
+    //STATES
+    /**
+     *
+     */
+    $scope.NORMAL_STATE = "grupo-camadas.normal";
+    /**
+     *
+     */
+    $scope.CONFIRM_STATE = "grupo-camadas.confirm";
+
+    /**
+     *
+     */
+	$scope.currentEntity;
+
+    /**
+     *
+     */
+    $scope.currentState;
+
+    /**
+     *
+     * @type {boolean}
+     */
+	var isEqual = false;
+
+	/*-------------------------------------------------------------------
+	 * 		 				 	  NAVIGATIONS
+	 *-------------------------------------------------------------------*/
+	/**
+	 * Método principal que faz o papel de front-controller da tela.
+	 * Ele é invocado toda vez que ocorre uma mudança de URL (@see $stateChangeSuccess),
+	 * quando isso ocorre, obtém o estado através do $state e chama o método inicial daquele estado.
+	 * Ex.: /list -> changeToList()
+	 *      /criar -> changeToInsert()
+	 *
+	 * Caso o estado não for encontrado, ele direciona para a listagem,
+	 * apesar que o front-controller do angular não deixa digitar uma URL inválida.
+	 */
+	$scope.initialize = function() 
+	{
+		$scope.currentEntity = new Attribute();
+	};
+
+	/*-------------------------------------------------------------------
+	 * 		 				 	  BEHAVIORS
+	 *-------------------------------------------------------------------*/
+
+
+	/**
+	 * 
+	 */
+	$scope.form = function( formName ) 
+	{
+
+		if ( !formName ) 
+		{
+			formName = "form";
+		}
+
+		return $("form[name="+formName+"]").scope()[formName];
+	};
+
+
+
+    /**
+     *
+     */
+    $scope.closeConfirm = function()
+    {
+        $scope.msg = null;
+        $scope.currentEntity.nome = '';
+        $scope.currentState = $scope.NORMAL_STATE;
+    };
+
+	/**
+	 *
+	 */
+	$scope.close = function() 
+	{
+		$scope.msg = null;
+		$modalInstance.close(null);
+	};
+};
