837a9644fb12aa025a42cb134b9d943f2a439b06
Thiago Rossetto Afonso <thiago.mx2012@gmail.com>
2014-11-20 14:05:09 -0200

    Arrumando problema do datepicker no IE 8

--- implementation/solution/src/main/webapp/modules/abstract-crud-controller.js
+++ implementation/solution/src/main/webapp/modules/abstract-crud-controller.js
@@ -56,6 +56,26 @@ function AbstractCRUDController( $scope, $log, $state, $rootScope, $timeout ) {
 
         return $("form[name="+formName+"]").scope()[formName];
     };
+    
+    $scope.detectIE = function() {
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf('MSIE ');
+        var trident = ua.indexOf('Trident/');
+
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        if (trident > 0) {
+            // IE 11 (or newer) => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        // other browser
+        return false;
+    }
 
     /**
      * Removemos as mensagens por este método
