d4d4a487046424c91bf90197cdde5c9f6604ccdd
Lucas <lucas.aguiar@eits.com.br>
2014-12-11 11:13:48 -0200

    Ajustes na pesquisa personalizada, ao pesquisar um campo data.

--- implementation/solution/src/main/webapp/modules/map/controller/map-controller.js
+++ implementation/solution/src/main/webapp/modules/map/controller/map-controller.js
@@ -1576,6 +1576,21 @@ function MapController( $scope, $injector, $log, $state, $timeout, $modal, $loca
      */
      $scope.selectCustomSearch = function(customSearch) {
          $scope.currentCustomSearch = customSearch;
+         
+         $timeout(function(){
+	         $('.datepicker').datepicker({ 
+				dateFormat: 'dd/mm/yy',
+				dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+			    dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+			    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+			    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+			    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+			    nextText: 'Próximo',
+			    prevText: 'Anterior'
+			});	
+	
+			$('.datepicker').mask("99/99/9999");
+         });
     };
     
     /**
