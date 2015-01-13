e9328200b426640b3fdec10533ae9c0eeca6126c
Thiago Rossetto Afonso <thiago.mx2012@gmail.com>
2015-01-13 17:07:11 -0200

    Adicionando testes

---
+++ implementation/solution/src/test/java/br/com/geocab/tests/service/SimpleTest.java
@@ -0,0 +1,28 @@
+package br.com.geocab.tests.service;
+
+import org.junit.Test;
+
+import br.com.geocab.domain.service.AccountService;
+import br.com.geocab.tests.AbstractIntegrationTest;
+
+/**
+ *
+ * @author Cristiano Correa
+ * @since 09/05/2013
+ * @version 1.0
+ * @category
+ */
+public class SimpleTest extends AbstractIntegrationTest
+{
+	/*-------------------------------------------------------------------
+	 *				 		     ATTRIBUTES
+	 *-------------------------------------------------------------------*/
+	
+	/*-------------------------------------------------------------------
+	 *				 		     	TESTS
+	 *-------------------------------------------------------------------*/
+	@Test
+	public void test()
+	{
+	}
+}
