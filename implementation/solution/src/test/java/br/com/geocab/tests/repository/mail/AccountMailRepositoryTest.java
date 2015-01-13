67c4995002a454eda94da16a87a94c82e656c5d1
Thiago Rossetto Afonso <thiago.mx2012@gmail.com>
2015-01-13 17:07:11 -0200

    Adicionando testes

---
+++ implementation/solution/src/test/java/br/com/geocab/tests/repository/mail/AccountMailRepositoryTest.java
@@ -0,0 +1,44 @@
+package br.com.geocab.tests.repository.mail;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.geocab.domain.entity.account.IAccountMailRepository;
+import br.com.geocab.domain.entity.account.User;
+import br.com.geocab.tests.AbstractIntegrationTest;
+
+/**
+ * 
+ * @author rodrigo
+ * @since 09/05/2013
+ * @version 1.0
+ * @category
+ 
+ */
+public class AccountMailRepositoryTest extends AbstractIntegrationTest
+{
+	/*-------------------------------------------------------------------
+	 *				 		     ATTRIBUTES
+	 *-------------------------------------------------------------------*/	
+	/**
+	 * 
+	 */
+	@Autowired
+	private IAccountMailRepository accountMailRepository;
+
+	/*-------------------------------------------------------------------
+	 *				 		     TESTS
+	 *-------------------------------------------------------------------*/
+	/**
+	 * 
+	 */
+	@Test
+	public void sendRecoveryPassword()
+	{
+		final User user = new User();
+		user.setEmail("thiago.rossetto@eits.com.br");
+		user.setName("Thiago Rossetttttttto");
+		
+		this.accountMailRepository.sendRecoveryPassword( user );
+	}
+}
