094c91670082377093f0f38e5514bb748edf2f5f
Vinicius <vinicius.kawamoto@eits.com.br>
2015-01-15 10:15:46 -0200

    adicionado icones do status da avaliação de postagem criado serviços para aceitar ou recusar uma avaliação de postagem

--- implementation/solution/src/main/java/br/com/geocab/domain/service/MarkerModerationService.java
+++ implementation/solution/src/main/java/br/com/geocab/domain/service/MarkerModerationService.java
@@ -3,17 +3,19 @@
  */
 package br.com.geocab.domain.service;
 
+import java.util.logging.Logger;
+
 import java.util.List;
 
 import org.directwebremoting.annotations.RemoteProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
-import br.com.geocab.domain.entity.datasource.DataSource;
+import br.com.geocab.domain.entity.marker.MarkerStatus;
 import br.com.geocab.domain.entity.markermoderation.MarkerModeration;
 import br.com.geocab.domain.repository.markermoderation.IMarkerModerationRepository;
 
@@ -34,6 +36,11 @@ public class MarkerModerationService
 	/*-------------------------------------------------------------------
 	 * 		 					ATTRIBUTES
 	 *-------------------------------------------------------------------*/
+	
+	/**
+	 * Log
+	 */
+	private static final Logger LOG = Logger.getLogger( MarkerModeration.class.getName() );
 	
 	/**
 	 * 
@@ -60,6 +67,52 @@ public class MarkerModerationService
 	{
 		return this.markerModerationRepository.listByFilters(filter, pageable);
 	}
+	
+	
+	/**
+	 * 
+	 * Method to accept a {@link MarkerModeration}
+	 * 
+	 * @param markerModeration
+	 * @return
+	 */
+	public MarkerModeration acceptMarkerModeration( MarkerModeration markerModeration )
+	{			
+		try
+		{
+			markerModeration.setStatus(MarkerStatus.ACCEPTED);
+			markerModeration = this.markerModerationRepository.save(markerModeration);
+		}
+		catch ( DataIntegrityViolationException e )
+		{
+			LOG.info( e.getMessage() );
+		}
+		
+		return markerModeration;
+	}
+	
+	/**
+	 * 
+	 * Method to refuse a {@link MarkerModeration}
+	 * 
+	 * @param markerModeration
+	 * @return
+	 */
+	public MarkerModeration refuseMarkerModeration( MarkerModeration markerModeration )
+	{			
+		try
+		{
+			markerModeration.setStatus(MarkerStatus.REFUSED);
+			markerModeration = this.markerModerationRepository.save(markerModeration);
+		}
+		catch ( DataIntegrityViolationException e )
+		{
+			LOG.info( e.getMessage() );
+		}
+		
+		return markerModeration;
+	}
+	
 	
 	/**
 	 *
