7a7debf58f88fc634d133b0bf4ccfcfad4172e03
Joaz Soares <joaz@cloudsource.com.br>
2015-05-25 21:01:47 -0300

    Criação de novos serviços restful

--- implementation/solution/src/main/java/br/com/geocab/application/restful/LayerGroupRESTful.java
+++ implementation/solution/src/main/java/br/com/geocab/application/restful/LayerGroupRESTful.java
@@ -1,13 +1,20 @@
 package br.com.geocab.application.restful;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import br.com.geocab.domain.entity.datasource.DataSource;
+import br.com.geocab.domain.entity.layer.Attribute;
 import br.com.geocab.domain.entity.layer.Layer;
 import br.com.geocab.domain.service.LayerGroupService;
 
@@ -62,6 +69,46 @@ public class LayerGroupRESTful
 		
 		return layers;
 	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value="internal/layers", method = RequestMethod.GET)
+	public @ResponseBody List<Layer> listAllInternalLayerGroups()
+	{
+		return this.layerGroupService.listAllInternalLayerGroups();
+	}	
+	
+	/**
+	 * 
+	 * @param layerId
+	 * @return
+	 */
+	@RequestMapping(value="/{layerId}/layerattributes", method = RequestMethod.GET)
+	public @ResponseBody List<Attribute> listAttributeByMarker(@PathVariable long layerId)
+	{
+		return this.layerGroupService.listAttributesByLayer(layerId);
+	}
+	
+	/**
+	 * 
+	 * @param listUrls
+	 * @return
+	 * @throws UnsupportedEncodingException 
+	 */
+	@RequestMapping(value="/layerproperties", method = RequestMethod.POST)
+	public @ResponseBody List<String> listAllFeatures(@RequestBody List<DataSource> dataSources) throws UnsupportedEncodingException
+	{
+		List<String> listUrls = new ArrayList<>();
+		
+		for ( DataSource source : dataSources)
+		{
+			listUrls.add(source.getUrl());
+		}
+		
+		return this.layerGroupService.listAllFeatures(listUrls);
+	}	
 	
 	
 }
--- implementation/solution/src/main/java/br/com/geocab/application/restful/MarkerRESTful.java
+++ implementation/solution/src/main/java/br/com/geocab/application/restful/MarkerRESTful.java
@@ -1,20 +1,29 @@
 package br.com.geocab.application.restful;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.GregorianCalendar;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
+import javax.jcr.RepositoryException;
+
+import org.directwebremoting.io.FileTransfer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.geocab.domain.entity.marker.Marker;
 import br.com.geocab.domain.entity.marker.MarkerAttribute;
 import br.com.geocab.domain.service.MarkerService;
+
+import com.vividsolutions.jts.io.WKTWriter;
 
 
 /**
@@ -50,7 +59,16 @@ public class MarkerRESTful
 	@RequestMapping(value="/{layerId}/markers", method = RequestMethod.GET)
 	public @ResponseBody List<Marker> listMarkerByLayer(@PathVariable long layerId)
 	{
-		return this.markerService.listMarkerByLayer(layerId);
+		List<Marker> markers = this.markerService.listMarkerByLayer(layerId);
+		final WKTWriter writer = new WKTWriter();
+		
+		// Pega a localização do objeto e converte para o formato WKT
+		for (Marker marker : markers)
+		{
+		    marker.setWktCoordenate(writer.write(marker.getLocation()));
+		}
+		
+		return markers;
 	}
 	
 	/**
@@ -63,5 +81,80 @@ public class MarkerRESTful
 	{
 		return this.markerService.listAttributeByMarker(markerId);
 	}
+	
+	/**
+	 * 
+	 * @param marker
+	 * @return
+	 * @throws RepositoryException 
+	 * @throws IOException 
+	 */
+	@RequestMapping(value="/", method = RequestMethod.POST)
+	public @ResponseBody Marker insertMarker(@RequestBody Marker marker) throws IOException, RepositoryException
+	{
+		return this.markerService.insertMarker(marker);
+	}
+	
+	/**
+	 * 
+	 * @param marker
+	 * @return
+	 * @throws RepositoryException 
+	 * @throws IOException 
+	 */
+	@RequestMapping(value="/", method = RequestMethod.PUT)
+	public @ResponseBody Marker updateMarker(@RequestBody Marker marker) throws IOException, RepositoryException
+	{
+		return this.markerService.updateMarker(marker);
+	}	
+	
+	/**
+	 * @param markerId
+	 */
+	@RequestMapping(value="/{markerId}", method = RequestMethod.DELETE)
+	public @ResponseBody void removeMarker(@PathVariable long markerId)
+	{
+		this.markerService.removeMarker(markerId);
+	}		
+	
+	/**
+	 * 
+	 * @param markerId
+	 */
+	@RequestMapping(value="/{markerId}/approve", method = RequestMethod.PUT)
+	public @ResponseBody void approveMarker(@PathVariable long markerId)
+	{
+		this.markerService.enableMarker(markerId);
+	}	
+	
+	/**
+	 * @param markerId
+	 */
+	@RequestMapping(value="/{markerId}/refuse", method = RequestMethod.PUT)
+	public @ResponseBody void refuseMarker(@PathVariable long markerId)
+	{
+		this.markerService.disableMarker(markerId);
+	}
+	
+	/**
+	 * 
+	 * @param file
+	 * @param markerId
+	 * @return
+	 */
+    @RequestMapping(value="/{markerId}/uploadphoto", headers="content-type=multipart/*", method=RequestMethod.POST)
+    public @ResponseBody String markerUploadPhoto(MultipartFile file, @PathVariable long markerId){
+        try 
+        {
+            byte[] bytes = file.getBytes();
+            FileTransfer fileTransfer = new FileTransfer(file.getOriginalFilename(), "image/jpeg", bytes);
+            this.markerService.uploadImg(fileTransfer, markerId);
+            return "Uploaded";
+        } 
+        catch ( Exception e )
+        {
+        	return e.getMessage();
+        }
+    }	
 	
 }
