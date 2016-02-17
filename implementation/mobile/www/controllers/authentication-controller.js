15045d497ed11892d9bce51ba826997fd600b5fe
Eduardo Alves <eduardo.alves@eits.com.br>
2016-02-17 09:56:22 -0200

    Ajustes nas configurações do ambiente mobile para desenvolvimento

--- implementation/mobile/www/controllers/authentication-controller.js
+++ implementation/mobile/www/controllers/authentication-controller.js
@@ -1,60 +1,63 @@
-(function ( angular ) {
-    'use strict';
+(function (angular) {
+  'use strict';
 
-/**
- *
- * @param $scope
- * @param $state
- */
-angular.module('application')
-	   .controller('AuthenticationController', function( $scope, $state, $http, $window, $ionicPopup, $API_ENDPOINT ) {
-		   
-    /*-------------------------------------------------------------------
-     * 		 				 	ATTRIBUTES
-     *-------------------------------------------------------------------*/
-     /**
-      * 
-      */
-     $scope.model = {
-         form: null,
-		 user: null,
-	 };
+  /**
+   *
+   * @param $scope
+   * @param $state
+   */
+  angular.module('application')
+    .controller('AuthenticationController', function ($scope, $state, $http, $window, $ionicPopup, $API_ENDPOINT) {
 
-    /*-------------------------------------------------------------------
-     * 		 				 	  HANDLERS
-     *-------------------------------------------------------------------*/
-   /**
-    * 
-    */
-	$scope.signinHandler = function() {
-		
-    	if ( $scope.model.form.$invalid ) {
-    		$ionicPopup.alert({
-    			title: 'Opss...',
-    		    subTitle: 'Os campos estão inválidos.',
-    		    template: 'Por favor verifique e tente novamente.'
-    		});
+      /*-------------------------------------------------------------------
+       * 		 				 	ATTRIBUTES
+       *-------------------------------------------------------------------*/
+      /**
+       *
+       */
+      $scope.model = {
+        form: null,
+        user: {}
+      };
 
-    	} else {
+      $scope.model.user.email = 'tbecker@itaipu.gov.br';
+      $scope.model.user.password = 'itaipu';
 
-			var config = {
-				headers: {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
-			};
-			
-			$http.post( $API_ENDPOINT+"/authenticate", $.param($scope.model.user), config)
-				.success( function( data, status, headers, config ) {
-					$state.go("home");
-				})
-				.error( function( data, status, headers, config ){
-					$ionicPopup.alert({
-		    			title: 'Opss...',
-		    		    subTitle: 'Não foi possível autenticar.',
-		    		    template: (data && data.message) ? data.message : data
-		    		});
-				}
-		    );
-    	}
-    }
-});
+      /*-------------------------------------------------------------------
+       * 		 				 	  HANDLERS
+       *-------------------------------------------------------------------*/
+      /**
+       *
+       */
+      $scope.loginHandler = function () {
+
+        if ($scope.model.form.$invalid) {
+          $ionicPopup.alert({
+            title: 'Opss...',
+            subTitle: 'Os campos estão inválidos.',
+            template: 'Por favor verifique e tente novamente.'
+          });
+
+        } else {
+
+          var config = {
+            headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+          };
+
+          $http.post($API_ENDPOINT + "/j_spring_security_check", $.param($scope.model.user), config)
+            .success(function (data, status, headers, config) {
+              $state.go("home");
+            })
+            .error(function (data, status, headers, config) {
+                $ionicPopup.alert({
+                  title: 'Opss...',
+                  subTitle: 'Não foi possível autenticar.',
+                  template: (data && data.message) ? data.message : data
+                });
+              }
+            );
+        }
+      }
+    });
 
 }(window.angular));
--- implementation/mobile/www/controllers/home-controller.js
+++ implementation/mobile/www/controllers/home-controller.js
@@ -1,64 +1,64 @@
-(function ( angular ) {
-    'use strict';
-    
-/**
- * 
- * @param $scope
- * @param $state
- */
-angular.module('application')
-	   .controller('HomeController', function( $rootScope, $scope, $state, $importService, $ionicPopup ) {
-	
-   /**
-    * 
-    */
-    $importService("accountService");
+(function (angular) {
+  'use strict';
 
-    /*-------------------------------------------------------------------
-     * 		 				 	ATTRIBUTES
-     *-------------------------------------------------------------------*/
-    /**
-     * 
-     */
-    $scope.model = {
-    	user: null,
-    };
+  /**
+   *
+   * @param $scope
+   * @param $state
+   */
+  angular.module('application')
+    .controller('HomeController', function ($rootScope, $scope, $state, $importService, $ionicPopup) {
 
-	/*-------------------------------------------------------------------
-     * 		 				  	POST CONSTRUCT
-     *-------------------------------------------------------------------*/
-    /**
-     * 
-     */
-    $scope.findUserById = function() {
-    	
-    	accountService.findUserById(1, {
-            callback: function (result) {
-            	
-            	$scope.model.user = result;
-            	
-            	$ionicPopup.alert({
-        			title: 'Serviço executado com sucesso',
-        		    template: ':D'
-        		});
-            	
-            	$scope.$apply();
-            },
-            errorHandler: function (message, exception) {
-        		$ionicPopup.alert({
-        			title: 'Opss...',
-        		    template: message
-        		});
-        		
-        		$scope.$apply();
-            }
+      /**
+       *
+       */
+      $importService("accountService");
+
+      /*-------------------------------------------------------------------
+       * 		 				 	ATTRIBUTES
+       *-------------------------------------------------------------------*/
+      /**
+       *
+       */
+      $scope.model = {
+        user: null
+      };
+
+      /*-------------------------------------------------------------------
+       * 		 				  	POST CONSTRUCT
+       *-------------------------------------------------------------------*/
+      /**
+       *
+       */
+      $scope.findUserById = function () {
+
+        accountService.findUserById(1, {
+          callback: function (result) {
+
+            $scope.model.user = result;
+
+            $ionicPopup.alert({
+              title: 'Serviço executado com sucesso',
+              template: ':D'
+            });
+
+            $scope.$apply();
+          },
+          errorHandler: function (message, exception) {
+            $ionicPopup.alert({
+              title: 'Opss...',
+              template: message
+            });
+
+            $scope.$apply();
+          }
         });
-    	
-    };
-	
-    /*-------------------------------------------------------------------
-     * 		 				 	  HANDLERS
-     *-------------------------------------------------------------------*/
-});
+
+      };
+
+      /*-------------------------------------------------------------------
+       * 		 				 	  HANDLERS
+       *-------------------------------------------------------------------*/
+    });
 
 }(window.angular));
