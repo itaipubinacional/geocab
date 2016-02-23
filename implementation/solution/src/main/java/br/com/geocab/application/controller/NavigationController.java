a056a5481944189a59a11af775393ad9dd88715e
emanuel.fonseca <emanuel.fonseca@eits.com.br>
2016-02-23 09:33:06 -0300

    Criando serviço de validação de access token

--- implementation/solution/src/main/java/br/com/geocab/application/controller/NavigationController.java
+++ implementation/solution/src/main/java/br/com/geocab/application/controller/NavigationController.java
@@ -1,20 +1,36 @@
 package br.com.geocab.application.controller;
 
+import java.util.Collection;
 import java.util.Locale;
 import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.geocab.application.ResourceBundleMessageSource;
+import br.com.geocab.domain.entity.account.User;
+import br.com.geocab.domain.repository.account.IUserRepository;
 
 /**
  * 
@@ -106,4 +122,91 @@ public class NavigationController
 	{
         return this.messageSource.getProperties( locale );
     }
+	
+//	/**
+//	 * 
+//	 */
+//	@Autowired
+//	private AuthenticationManager authenticationManager;
+	
+	/**
+	 * 
+	 */
+	@Autowired
+	private IUserRepository userDetailsService;
+	
+	/**
+	 * 
+	 * @param request
+	 * @param userName
+	 * @param token
+	 */
+	@RequestMapping(value="/validateToken/{userName}/{token}", method = RequestMethod.GET)
+	public @ResponseBody String validateToken(HttpServletRequest request, @PathVariable String userName, @PathVariable String token)
+	{
+		this.validateToken(token);
+		
+		UserDetails user = userDetailsService.loadUserByUsername(userName);
+		
+	    UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+
+	    CustomAuthenticationProvider pro = new CustomAuthenticationProvider(user);
+	    
+	    // Authenticate the user
+	    Authentication authentication =  pro.authenticate(authRequest);
+	    SecurityContext securityContext = SecurityContextHolder.getContext();
+	    
+	    securityContext.setAuthentication(authentication);
+	    
+	    // Create a new session and add the security context.
+	    HttpSession session = request.getSession(true);
+	    session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+	    
+	    return "Token valid";
+	}
+	
+	/**
+	 * 
+	 * @param token
+	 */
+	private void validateToken(String token)
+	{
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getForObject("https://graph.facebook.com/me?access_token="+token, String.class);
+	}
+	
+	public class CustomAuthenticationProvider implements AuthenticationProvider {
+		 
+		/**
+		 * 
+		 */
+		private UserDetails user;
+	 
+	    /**
+		 * @param user
+		 */
+		public CustomAuthenticationProvider(UserDetails user)
+		{
+			super();
+			this.user = user;
+		}
+
+		/**
+		 * 
+		 */
+		@Override
+	    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+	        return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+	    }
+		
+		/**
+		 * 
+		 */
+	    @Override
+	    public boolean supports(Class<?> arg0) {
+	        return true;
+	    }
+	}
+	
+	
 }
