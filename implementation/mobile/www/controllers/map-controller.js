8dd22b6b2cebfe17e6bf8bc14f6dbb4de6fb4d67
emanuel.fonseca <emanuel.fonseca@eits.com.br>
2016-02-29 10:41:32 -0300

    Feito handler para pegar camadas externas

--- implementation/mobile/www/controllers/map-controller.js
+++ implementation/mobile/www/controllers/map-controller.js
@@ -503,9 +503,9 @@
       $scope.listAllInternalLayerGroups = function () {
 
         if ($scope.allInternalLayerGroups.length == 0) {
-          layerGroupService.listAllInternalLayerGroups({
+          layerGroupService.listLayersByFilters(null, null, {
             callback: function (result) {
-              $scope.allInternalLayerGroups = result;
+              $scope.allInternalLayerGroups = result.content;
 
               $scope.allInternalLayerGroups[2].visible = true;
               $scope.toggleLayer($scope.allInternalLayerGroups[2]);
@@ -996,7 +996,8 @@
         localStorage.removeItem('userEmail');
         localStorage.removeItem('token');
         $location.path($rootScope.$API_ENDPOINT + "/j_spring_security_logout");
-
+        
+        $scope.userMe = {};
         $rootScope.model = {
           form: null,
           user: {
