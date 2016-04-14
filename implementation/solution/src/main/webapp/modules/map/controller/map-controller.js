e4554ac6a370bd95728f52e43b6de3ce592547bc
Eduardo Alves <eduardo.alves@eits.com.br>
2016-04-14 18:39:11 -0300

    Tradução da mensagem de coordenadas inválidas ao salvar ponto

--- implementation/solution/src/main/webapp/modules/map/controller/map-controller.js
+++ implementation/solution/src/main/webapp/modules/map/controller/map-controller.js
@@ -3730,7 +3730,7 @@ function MapController($scope, $injector, $log, $state, $timeout, $modal, $locat
 
         $scope.isLoading = false;
 
-        $scope.msg = {type: "danger", text: message};
+        $scope.msg = {type: "danger", text: $translate(message), dismiss: true};
 
         $scope.currentEntity.layer = oldLayer;
 
