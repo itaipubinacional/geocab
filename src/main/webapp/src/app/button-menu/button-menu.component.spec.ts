e71aed5937938054aa81780ab01bc9187ca7577a
Thiago Becker <beckerthiago@gmail.com>
2017-03-02 11:51:18 -0300

    Adicionando arquivos iniciais da nova interface web.

---
+++ src/main/webapp/src/app/button-menu/button-menu.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ButtonMenuComponent } from './button-menu.component';
+
+describe('ButtonMenuComponent', () => {
+  let component: ButtonMenuComponent;
+  let fixture: ComponentFixture<ButtonMenuComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ButtonMenuComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ButtonMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
