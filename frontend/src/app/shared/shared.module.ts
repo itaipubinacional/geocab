6388f1a9b4151b68e50d739899a80665ce1ebad8
Luiz Carlos Viana Melo <lcvmelo@itaipu.gov.br>
2017-03-22 08:50:45 -0300

    Refatoração da interface de mapa para utilizar Bootstrap

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -6,20 +6,17 @@ import {HttpModule} from "@angular/http";
 import {UserService} from "./user.service";
 import {UserRouteGuard, ModeratorUserRouteGuard, AdminUserRouteGuard} from "./user.route-guards";
 import {TreeViewComponent} from "./tree-view/tree-view.component";
-import {MaterializeModule} from "angular2-materialize";
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
-        HttpModule,
-        MaterializeModule
+        HttpModule
     ],
     exports: [
         CommonModule,
         RouterModule,
         HttpModule,
-        MaterializeModule,
         NavComponent,
         TreeViewComponent
     ],
