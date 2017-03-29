9edd0ba10fe9327436ecfbae6f0887044b96ee35
Luiz Carlos Viana Melo <lcvmelo@itaipu.gov.br>
2017-03-29 14:52:26 -0300

    Implementação da chamada do serviço de configuração no front-end

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,17 +1,21 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
 import {UserService} from "./user.service";
 import {UserRouteGuard, ModeratorUserRouteGuard, AdminUserRouteGuard} from "./user.route-guards";
 import {OAuthModule} from "angular-oauth2-oidc";
+import {HttpModule} from "@angular/http";
+import {ConfigService} from "./config.service";
 
 @NgModule({
     imports: [
         CommonModule,
+        HttpModule,
         OAuthModule.forRoot()
     ],
     declarations: [],
     providers: [
         UserService,
+        ConfigService,
         UserRouteGuard,
         ModeratorUserRouteGuard,
         AdminUserRouteGuard
